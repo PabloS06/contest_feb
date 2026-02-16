fcc349278a79011d09c59c148a5e89bf72c7cee9
Pablo_Solimeo <pablosolim@gmail.com>
2026-02-16 16:21:37 +0100

    Challenge

--- decode_start.py
+++ decode_start.py
@@ -1,6 +1,14 @@
-alphabet = " abcdefghijklmnopqrstuvwxyz"
+alphabet = " abcdefghijklmnopqrstuvwxyz"   # index: 0=' ', 1='a', ... 26='z'
 vowel = "aeiou"
 message = ""
+
 with open("secret.txt") as f:
     for line in f:
-        print(line) # instead of print, you should start decoding
+        nr_vowels = 0
+        for v in vowel:
+            nr_vowels += line.count(v) # count the num. of times that a particular vowel shows up
+        letter = alphabet[nr_vowels]
+        message += alphabet[nr_vowels]
+
+    print(message)
+
